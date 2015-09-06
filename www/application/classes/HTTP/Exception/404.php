7a7c0ee7f3007a9459749a2efc782d9fed58fc1a
= <programist1985@gmail.com>
2015-09-06 17:15:41 +0300

    Add 404 error page

---
+++ www/application/classes/HTTP/Exception/404.php
@@ -0,0 +1,37 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+	/**
+	 * Класс обработки 404 ошибки
+	 *
+	 * @name        HTTP_Exception_404
+	 * @category    HTTP
+	 * @subcategory Exception
+	 * @author      Суслин Денис 2015г. <programist1985@gmail.com>
+	 */
+	 
+	class HTTP_Exception_404 extends Kohana_HTTP_Exception_404 {
+	 
+		/**
+		 * Generate a Response for the 404 Exception.
+		 *
+		 * The user should be shown a nice 404 page.
+		 * 
+		 * @return Response
+		 */
+		public function get_response() {
+	
+			$view = View::factory(Config::getSiteParam('site_template') . '/errors/404');
+	 
+			// Remembering that `$this` is an instance of HTTP_Exception_404
+			$view -> message = $this -> getMessage();
+	 
+			$response = Response::factory()
+				-> status(404)
+				-> body($view -> render());
+	 
+			return $response;
+		}
+	}
+
+    /* End of file 404.php */
+    /* Location: ./application/classes/HTTP/Exception/404.php */
