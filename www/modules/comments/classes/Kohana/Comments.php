6d07cc0ea6c312adeb003f4846c5bd7242cde088
= <programist1985@gmail.com>
2015-09-06 16:20:41 +0300

    Fix comments

--- www/modules/comments/classes/Kohana/Comments.php
+++ www/modules/comments/classes/Kohana/Comments.php
@@ -1,20 +1,50 @@
-<?php defined('SYSPATH') or die('No direct script access.'); // как обычно - защита от прямого доступа
+<?php defined('SYSPATH') or die('No direct script access.'); 
 
-class Kohana_Comments { // поехали!
-	
-	public static function factory(array $config = array()) // здесь происходит создание объекта
-	{
-		return new Comments($config); // создаем новый объект Comments с нашим конфигом
+	/**
+	 * Модуль коментарией VK
+	 *
+	 * @name        Kohana_Comments
+	 * @category    Modules
+	 * @subcategory Comments
+	 * @author      Суслин Денис 2015г. <programist1985@gmail.com>
+	 */
+
+	class Kohana_Comments { 
+		
+		/**
+		 * Конструктор класса
+		 */
+		public function __construct(array $config = array()) {
+			
+			// заносим в $this -> config конфиг из папки с модулем и объединяем его с конфигом пользователя, елси он есть
+			$this -> config = Kohana::$config -> load('comments') -> as_array(); 
+		}
+		
+		/**
+		 * Фабричный метод для класса
+		 *
+		 * @param array $config конфигурация
+		 * @return object
+		 */
+		public static function factory(array $config = array()) {
+			
+			return new Comments($config); 
+		}
+
+		/**
+		 * Метод отрисовки комментариев
+		 *
+		 * @return html
+		 */	
+		public function render() {
+			
+			// создаем переменную с нашим видом, в который передаем конфиг
+			$view = View::factory('comments/comments') -> set('cfg', $this -> config); 
+			
+			// как результат вызова функции - возвращаем отрендеренный вид
+			return $view -> render(); 
+		}
 	}
 	
-	public function __construct(array $config = array()) // конструктор класса
-	{
-		$this->config = Kohana::$config->load('comments')->as_array(); // заносим в $this->config конфиг из папки с модулем и объединяем его с конфигом пользователя, елси он есть
-	}
-	
-	public function render() // функция рисования комментариев
-	{	
-		$view = View::factory('comments/comments')->set('cfg', $this->config); // создаем переменную с нашим видом, в который передаем конфиг
-		return $view->render(); // как результат вызова функции - возвращаем отрендеренный вид
-	}
-}
+    /* End of file Comments.php */
+    /* Location: ./modules/comments/classes/Kohana/Comments.php */
--- www/modules/comments/views/comments/comments.php
+++ www/modules/comments/views/comments/comments.php
@@ -1,16 +1,23 @@
 <hr />
 <div class="row">
-	<div class="span6">
+	<div class="col-lg-6">
 		<div id="vk_comments"></div>
 		<script type="text/javascript" src="//vk.com/js/api/openapi.js?717"></script>
 		<script type="text/javascript">
-		  VK.init({apiId: <?=$cfg['vk_key'];?>, onlyWidgets: true});
+			VK.init({ apiId: <?php echo $cfg[ 'vk_key' ]; ?>, onlyWidgets: true});
 		</script>
 		<script type="text/javascript">
-		VK.Widgets.Comments("vk_comments", {limit: <?=$cfg['vk_messages'];?>, width: "<?=$cfg['vk_width'];?>", attach: "*"});
+			VK.Widgets.Comments("vk_comments", {
+				limit: <?php echo $cfg[ 'vk_messages' ]; ?>, 
+				width: "<?php echo $cfg[ 'vk_width' ]; ?>", 
+				attach: "*"
+			});
 		</script>
 	</div>
-	<div class="span6">
-		<div class="fb-comments" data-href="<?=$_SERVER['HTTP_HOST'];?>/<?=$_SERVER['REQUEST_URI'];?>" data-width="<?=$cfg['fb_width'];?>" data-num-posts="<?=$cfg['fb_messages'];?>"></div>
+	<div class="col-lg-6">
+		<div class="fb-comments" 
+			data-href="<?php echo $_SERVER[ 'HTTP_HOST' ]; ?>/<?php echo $_SERVER[ 'REQUEST_URI' ]; ?>" 
+			data-width="<?php echo $cfg[ 'fb_width' ]; ?>" 
+			data-num-posts="<?php echo $cfg[ 'fb_messages' ]; ?>"></div>
 	</div>
 </div>
